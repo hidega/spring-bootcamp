a264809e1201c25698e98558b3ad6e40f09d711b
Andras Hideg <andrashideg73@gmail.com>
2026-05-12 13:18:47 +0200

    dev

--- docs/csheet/build.js
+++ docs/csheet/build.js
@@ -1,8 +1,6 @@
 const fs = require('node:fs/promises'); 
 const process = require('node:process');
-const topicsDir = './topics';
-
-let idCounter = 0;
+const topicsDir = './topics'; 
 
 const createTopic = (s) => {
   const first = s.indexOf('\n');
@@ -13,7 +11,7 @@ const createTopic = (s) => {
   topic.title = s.substring(first, second).trim();
   topic.tags = s.substring(second, third).trim();
   topic.text = s.substring(third);
-  topic.id = ++idCounter;
+  topic.id = (topic.chapter + '-' + topic.title).replaceAll(' ', '_');
   return topic;
 };
 
@@ -38,22 +36,42 @@ const createDocument = (ts) => {
 
 const appendTopic = (n, t, h) => h + 
   `<div data-tags="${t.tags}" class="cs-paragraph" id="${t.id}">` +
-  `<div class="cs-title">${t.title}</div><div class="cs-text">${t.text}</div>`; 
+  `<div class="cs-title">${t.title}</div><div class="cs-text">${t.text}</div></div>\n`; 
 
 const appendChapter = (n, c, h) => h + 
-  `<div class="cs-chapter" data-chapter-name="${n}">` + 
+  `<div class="cs-chapter" data-chapter-name="${n}"><div class="cs-title">${n}</div>` +  
   Object.keys(c).reduce((a, k) => appendTopic(k, c[k], a), '') + 
-  '</div>';
+  '</div>\n';
 
 const htmlize = (d) => '<div class="cs-bg">' + 
   Object.keys(d).reduce((a, k) => appendChapter(k, d[k], a), '') + 
-  '</div>';
+  '</div>\n';
+
+const createHtmlPage = (c) => 
+`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>CSheet</title>
+    <link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="styles.css" type="text/css"/>
+    <script type="text/javascript" src="csheet.js"></script>
+  </head>
+  <body>
+    <div class="cs-bg">
+
+${c}
+    </div>
+  </body>
+</html>
+`;
 
 fs.readdir(topicsDir)
   .then((fa) => Promise.all(fa.reduce(readFile, [])))
   .then((bs) => bs.map((b) => createTopic(b.toString('utf-8'))))
   .then(createDocument)
   .then(htmlize)
-  .then(console.log)
+  .then(createHtmlPage)
+  .then((s) => fs.writeFile('./index.html', s))
   .then(() => console.log('Success :)'))
   .catch(exitError);
